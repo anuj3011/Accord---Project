2b9f050e8ddb5c80e144dadf0be471673005ea87
Anuj Agrawal <anuj.agwl30@gmail.com>
2020-09-14 23:14:38 +0530

    OTP VERIFICATION PAGE

---
+++ Accord/app/src/main/java/com/example/accord/Auth/EnterOTP.java
@@ -0,0 +1,16 @@
+package com.example.accord.Auth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.accord.R;
+
+public class EnterOTP extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enter_o_t_p);
+    }
+}
