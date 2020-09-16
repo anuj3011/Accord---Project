5b616ad8a923edbd122544d720fbf241cdbe430a
Anuj Agrawal <anuj.agwl30@gmail.com>
2020-09-16 09:49:20 +0530

    LOGIN MODULE without variable declaration

---
+++ Accord/app/src/main/java/com/example/accord/Auth/UserType.java
@@ -0,0 +1,16 @@
+package com.example.accord.Auth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.accord.R;
+
+public class UserType extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_type);
+    }
+}
