7c9e2502ab58d64db88ff57b918a2c4487b5789a
Anuj Agrawal <anuj.agwl30@gmail.com>
2020-09-17 17:24:02 +0530

    Input variables
    variables with user details made in RegisterService and RegisterUser class.

--- Accord/app/src/main/java/com/example/accord/Auth/RegisterService.java
+++ Accord/app/src/main/java/com/example/accord/Auth/RegisterService.java
@@ -3,6 +3,8 @@ package com.example.accord.Auth;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.accord.R;
 
@@ -13,4 +15,40 @@ public class RegisterService extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_service);
     }
+
+    public void registerService(View view){
+
+        String first_name=null,last_name=null;
+        int Age=0;
+        long Phone=0;
+
+        String Address=null,email=null,profession=null,password=null;
+
+        TextView textView = (TextView) findViewById(R.id.inputFirstName);
+        first_name =  textView.getText().toString();
+
+        textView = (TextView) findViewById(R.id.inputLastName);
+        last_name =  textView.getText().toString();
+
+        textView = (TextView) findViewById(R.id.inputAge);
+        Age= Integer.parseInt(textView.getText().toString());
+
+        textView = (TextView) findViewById(R.id.inputPhone);
+        Phone= Integer.parseInt(textView.getText().toString());
+
+        textView = (TextView) findViewById(R.id.inputAddress);
+        Address =  textView.getText().toString();
+
+        textView = (TextView) findViewById(R.id.inputEmail);
+        email =  textView.getText().toString();
+
+        textView = (TextView) findViewById(R.id.inputProfession);
+        profession =  textView.getText().toString();
+
+        textView = (TextView) findViewById(R.id.inputPass);
+        password =  textView.getText().toString();
+
+
+    }
 }
+
--- Accord/app/src/main/java/com/example/accord/Auth/RegisterUser.java
+++ Accord/app/src/main/java/com/example/accord/Auth/RegisterUser.java
@@ -3,6 +3,8 @@ package com.example.accord.Auth;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.accord.R;
 
@@ -12,5 +14,36 @@ public class RegisterUser extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_user);
+    }
+    public void registerUser(View view){
+        String name=null;
+        int Age=0,pincode;
+        long Phone=0;
+        String password=null;
+        String email=null, add1=null,area=null,city=null;
+
+
+        TextView textView = (TextView) findViewById(R.id.inputName);
+        name =  textView.getText().toString();
+
+        textView = (TextView) findViewById(R.id.inputNumber);
+        Phone =  Integer.parseInt(textView.getText().toString());
+
+        textView = (TextView) findViewById(R.id.inputEMAIL);
+        email=textView.getText().toString();
+
+        textView = (TextView) findViewById(R.id.inputAdd1);
+        add1=textView.getText().toString();
+
+        textView = (TextView) findViewById(R.id.inputArea);
+        area=textView.getText().toString();
+
+        textView = (TextView) findViewById(R.id.inputCity);
+        city=textView.getText().toString();
+
+        textView = (TextView) findViewById(R.id.inputPass);
+        password = textView.getText().toString();
+
+
     }
 }
