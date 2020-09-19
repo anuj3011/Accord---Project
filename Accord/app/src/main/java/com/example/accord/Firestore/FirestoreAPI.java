557918c840f5275c243f963e5bce65ffd458ab97
Dhruv <dhruv_devasthale@outlook.com>
2020-09-19 15:42:47 +0530

    firestore api class

---
+++ Accord/app/src/main/java/com/example/accord/Firestore/FirestoreAPI.java
@@ -0,0 +1,56 @@
+package com.example.accord.Firestore;
+
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FirestoreAPI {
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    void pullFirestore(String collection)  {
+        db.collection("donors")// collection reference
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {// callback when data is returned
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d("TAG", document.getId() + " => " + document.getData());
+                            }
+                        } else {
+                            Log.w("TAG", "Error getting documents.", task.getException());
+                        }
+                    }
+                });
+    }
+    void pushFirestore(String collection,Object object){
+        // Add a new document with a generated ID
+        Map<String,String> testData = new HashMap<>();
+        testData.put("name","testNgo");
+        db.collection(collection)
+                .add(testData)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d("TAG", "DocumentSnapshot added with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w("TAG", "Error adding document", e);
+                    }
+                });
+    }
+}
--- Accord/app/src/main/java/com/example/accord/MainActivity.java
+++ Accord/app/src/main/java/com/example/accord/MainActivity.java
@@ -28,43 +28,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
-    FirebaseFirestore db = FirebaseFirestore.getInstance();
 
-    void firestoreReadTest()  {
-        db.collection("donors")// collection reference
-                .get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {// callback when data is returned
-                        if (task.isSuccessful()) {
-                            for (QueryDocumentSnapshot document : task.getResult()) {
-                                Log.d("TAG", document.getId() + " => " + document.getData());
-                            }
-                        } else {
-                            Log.w("TAG", "Error getting documents.", task.getException());
-                        }
-                    }
-                });
-    }
-    void firestoreWriteTest(){
-        // Add a new document with a generated ID
-        Map<String,String> testData = new HashMap<>();
-        testData.put("name","testNgo");
-        db.collection("ngo")
-                .add(testData)
-                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                    @Override
-                    public void onSuccess(DocumentReference documentReference) {
-                        Log.d("TAG", "DocumentSnapshot added with ID: " + documentReference.getId());
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.w("TAG", "Error adding document", e);
-                    }
-                });
-    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
