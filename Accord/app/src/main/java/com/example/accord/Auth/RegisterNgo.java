ee3744ae7483524ba34fe2083ba3ad3c2c62aa79
Anuj Agrawal <anuj.agwl30@gmail.com>
2020-09-24 20:37:53 +0530

    NGO VARIABLE
    Ngo variables in registerNgo class, they are temporary but for now work with them.

--- Accord/app/src/main/java/com/example/accord/Auth/RegisterNgo.java
+++ Accord/app/src/main/java/com/example/accord/Auth/RegisterNgo.java
@@ -3,6 +3,8 @@ package com.example.accord.Auth;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.accord.R;
 
@@ -12,5 +14,40 @@ public class RegisterNgo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_ngo);
+    }
+
+    public void registerNGO(View view){
+
+        String full_name=null;
+
+        long Phone=0;
+
+        String Address=null,email=null,profession=null,password=null;
+
+        TextView textView = (TextView) findViewById(R.id.inputFirstName);
+        full_name =  textView.getText().toString();
+
+//        textView = (TextView) findViewById(R.id.inputLastName);
+//        last_name =  textView.getText().toString();
+
+//        textView = (TextView) findViewById(R.id.inputAge);
+//        Age= Integer.parseInt(textView.getText().toString());
+
+        textView = (TextView) findViewById(R.id.inputPhone);
+        Phone= Integer.parseInt(textView.getText().toString());
+
+        textView = (TextView) findViewById(R.id.inputAddress);
+        Address =  textView.getText().toString();
+
+        textView = (TextView) findViewById(R.id.inputEmail);
+        email =  textView.getText().toString();
+
+        textView = (TextView) findViewById(R.id.inputProfession);
+        profession =  textView.getText().toString();
+
+        textView = (TextView) findViewById(R.id.inputPass);
+        password =  textView.getText().toString();
+
+
     }
 }
