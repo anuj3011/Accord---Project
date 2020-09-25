486d3c89573300605578da97d4b4f161979db845
Dhruv <dhruv_devasthale@outlook.com>
2020-09-25 17:35:19 +0530

    new ui auth integration

--- Accord/app/src/main/java/com/example/accord/Auth/SignIn.java
+++ Accord/app/src/main/java/com/example/accord/Auth/SignIn.java
@@ -13,13 +13,10 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.accord.Firebase.EmailAuth;
-import com.example.accord.Firebase.GoogleAuth;
+
 import com.example.accord.R;
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInClient;
-import com.google.android.gms.common.api.ApiException;
+
+
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseUser;
@@ -28,18 +25,19 @@ import com.google.firebase.auth.FirebaseUser;
 //import com.google.android.material.snackbar.Snackbar;
 
 public class SignIn extends AppCompatActivity {
-    private   int RC_SIGN_IN = 1;
+    private int RC_SIGN_IN = 1;
     private long backPressedTime;
     private Toast backToast;
-    boolean emailSent=false;
-    int flag=0;
+    boolean emailSent = false;
+    int flag = 0;
     String email;
     String pass;
     EditText textInput;
-    EmailAuth emailAuth= new EmailAuth();
+    EmailAuth emailAuth = new EmailAuth();
     public Button signInButton;
-    GoogleAuth googleAuth= new GoogleAuth();
+    TextView newUserText;
     Activity self;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -51,8 +49,15 @@ public class SignIn extends AppCompatActivity {
         //Window g = getWindow();
         //g.setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS,WindowManager.LayoutParams.TYPE_STATUS_BAR);
         setContentView(R.layout.activity_sign_in);
-        self=this;
-        signInButton= (Button)findViewById(R.id.LoginButton) ;
+        self = this;
+        signInButton = (Button) findViewById(R.id.LoginButton);
+        newUserText = findViewById(R.id.NewUser);
+        newUserText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                NewUser();
+            }
+        });
         signInButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -66,66 +71,73 @@ public class SignIn extends AppCompatActivity {
     @Override
     public void onBackPressed() {
 
-        if(backPressedTime + 2000 > System.currentTimeMillis()) {
+        if (backPressedTime + 2000 > System.currentTimeMillis()) {
             backToast.cancel();
             super.onBackPressed();
             return;
-        }else
-        {
-            backToast= Toast.makeText(getBaseContext(), "Press back again to exit", Toast.LENGTH_SHORT);
-            backToast.show();;
+        } else {
+            backToast = Toast.makeText(getBaseContext(), "Press back again to exit", Toast.LENGTH_SHORT);
+            backToast.show();
+            ;
         }
         backPressedTime = System.currentTimeMillis();
     }
 
-    public void signIn(){
-        textInput=findViewById(R.id.EmailText);
-        email=textInput.getText().toString();
-        textInput=findViewById(R.id.PasswordText);
-        pass=textInput.getText().toString();
-        emailAuth.signIn(email,pass,this);
+    public void signIn() {
+        textInput = findViewById(R.id.Email);
+        email = textInput.getText().toString();
+        textInput = findViewById(R.id.Password);
+        pass = textInput.getText().toString();
+        emailAuth.signIn(email, pass, this);
 
     }
-    public void NewUser(View view){
+
+    public void NewUser() {
 
         signInButton.setText("Register");
 
         signInButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(emailSent){
-                    final FirebaseUser user=emailAuth.mAuth.getCurrentUser();
+                if (emailSent) {
+                    final FirebaseUser user = emailAuth.mAuth.getCurrentUser();
                     user.reload().addOnCompleteListener(new OnCompleteListener<Void>() {
                         @Override
                         public void onComplete(@NonNull Task<Void> task) {
-                            if(user.isEmailVerified()){
+                            if (user.isEmailVerified()) {
                                 Toast.makeText(getBaseContext(), "Verified", Toast.LENGTH_SHORT).show();
                                 // got to dashboard
-                                //navigate ahead
-                            }
-                            else{
+                                //navigate ahead to Profile Page
+                            } else {
                                 Toast.makeText(getBaseContext(), "Not Verified", Toast.LENGTH_SHORT).show();
                             }
                         }
                     });
 
-                }
-                else {
-                   //text input
-                    TextView textView = (TextView) findViewById(R.id.PasswordText);
-                    pass=textView.getText().toString();
-                    textView=findViewById(R.id.EmailText);
-                    email=textView.getText().toString();
-                    signInButton.setText("Check Verification");
-                    emailSent=true;
-                    emailAuth.registerUser(email,pass,self);
+                } else {
+                    //text input
+                    TextView textView = (TextView) findViewById(R.id.Email);
+                    email = textView.getText().toString();
+                    textView = findViewById(R.id.Password);
+                    pass = textView.getText().toString();
+
+                    if (email == null || email.length() < 1) {
+                        Toast.makeText(getBaseContext(), "Email Empty", Toast.LENGTH_SHORT).show();
+                    } else if (pass == null || pass.length() < 1) {
+                        Toast.makeText(getBaseContext(), "Password Empty", Toast.LENGTH_SHORT).show();
+
+                    } else {
+
+                        emailSent=true;
+                        emailAuth.registerUser(email, pass, self);
+                    }
+
 
                 }
 
-              // email link
+                // email link
             }
         });
-
 
 
     }
