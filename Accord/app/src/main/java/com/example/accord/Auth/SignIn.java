32f3703ecddbabd285eab67a44e5c28268b99f8a
Aditya88Gupta <prince88aditya@gmail.com>
2020-09-25 16:59:24 +0530

    New Sign In

--- Accord/app/src/main/java/com/example/accord/Auth/SignIn.java
+++ Accord/app/src/main/java/com/example/accord/Auth/SignIn.java
@@ -35,7 +35,7 @@ public class SignIn extends AppCompatActivity {
         //Window g = getWindow();
         //g.setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS,WindowManager.LayoutParams.TYPE_STATUS_BAR);
         setContentView(R.layout.activity_sign_in);
-        signInButton= (Button)findViewById(R.id.LoginButton) ;
+        //signInButton= (Button)findViewById(R.id.LoginButton) ;
 
 
     }
@@ -55,55 +55,5 @@ public class SignIn extends AppCompatActivity {
         backPressedTime = System.currentTimeMillis();
     }
 
-    public void signIn(){
-        textInput=findViewById(R.id.EmailText);
-        email=textInput.getText().toString();
-        textInput=findViewById(R.id.signInPass);
-        pass=textInput.getText().toString();
-        emailAuth.signIn(email,pass,this);
-
-    }
-    public void NewUser(View view){
-        TextView textView = (TextView) findViewById(R.id.PasswordText);
-        textView.animate().alpha(0).setDuration(500);
-        textView = (TextView) findViewById(R.id.NewUser);
-        textView.animate().alpha(0).setDuration(500);
-        textView = (TextView) findViewById(R.id.ForgotPass);
-        textView.setText("Or");
-        textView = (TextView) findViewById(R.id.TopText2);
-        textView.animate().alpha(0).setDuration(600);
-        textView = (TextView) findViewById(R.id.TopText3);
-        textView.animate().alpha(1).setDuration(600);
-        textView = (TextView) findViewById(R.id.GoogleText);
-        textView.animate().alpha(1).setDuration(600);
-        Button button = (Button) findViewById(R.id.LoginButton);
-        button.setText("Get OTP");
-        signInButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                TextView textView2 = (TextView) findViewById(R.id.OTP);
-                textView2.animate().alpha(1).setDuration(600);
-                textView2 = (TextView) findViewById(R.id.TopText3);
-                textView2.animate().alpha(0).setDuration(600);
-                textView2 = (TextView) findViewById(R.id.TopText4);
-                textView2.animate().alpha(1).setDuration(600);
-                textView2 = (TextView) findViewById(R.id.GoogleText);
-                textView2.animate().alpha(1).setDuration(600);
-                Button button = (Button) findViewById(R.id.LoginButton);
-                button.setText("Sign Up");
-                signInButton.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Intent intent = new Intent(getApplicationContext(), RegisterUser.class);
-                        startActivity(intent);
-                        finish();
-                    }
-                });
-            }
-        });
-
-
-
-    }
 
 }
