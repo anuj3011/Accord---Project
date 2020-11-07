9fb9a13ec1de7c3f8f3d1c98435ecdd96b0a22a1
Dhruv <dhruv_devasthale@outlook.com>
2020-11-08 03:32:43 +0530

    add polyline on map

--- Accord/app/src/main/java/com/example/accord/TrackOrder.java
+++ Accord/app/src/main/java/com/example/accord/TrackOrder.java
@@ -7,6 +7,7 @@ import androidx.core.app.ActivityCompat;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -23,6 +24,9 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 
 import java.util.List;
 
@@ -31,6 +35,7 @@ public class TrackOrder extends AppCompatActivity {
     LocationManager locationManager;
     CustomLatLng currentLocation;
     CustomLatLng serviceProviderLocation;
+    boolean alreadyHasLocation=false;
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
 
@@ -46,13 +51,18 @@ public class TrackOrder extends AppCompatActivity {
 
 
     }
-    void centerOnCurrentLocation(){
-        LatLng SelectedLocation = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
-        mMap.clear();
 
-        //mMap.addMarker(new MarkerOptions().position(SelectedLocation).title(title));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(SelectedLocation, 15));
+    void centerOnCurrentLocation() {
+        if(!alreadyHasLocation){
+            LatLng SelectedLocation = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
+            mMap.clear();
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(SelectedLocation, 15));
+            showRoute();
+            alreadyHasLocation=true;
+        }
+
     }
+
     void requestLocationUpdates() {
         locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
@@ -67,15 +77,16 @@ public class TrackOrder extends AppCompatActivity {
         }
         Log.d("location", "getting location");
         mMap.setMyLocationEnabled(true);
+
         locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, new LocationListener() {
             @Override
             public void onLocationChanged(@NonNull Location location) {
                 Log.d("location", location.toString());
-                currentLocation=new CustomLatLng();
-                currentLocation.latitude=location.getLatitude();
-                currentLocation.longitude=location.getLongitude();
+                currentLocation = new CustomLatLng();
+                currentLocation.latitude = location.getLatitude();
+                currentLocation.longitude = location.getLongitude();
                 centerOnCurrentLocation();
-                showRoute();
+
 
             }
 
@@ -96,15 +107,22 @@ public class TrackOrder extends AppCompatActivity {
         }
 
     }
-    void showRoute(){
-   //     LatLng serviceProviderLocation=new LatLng(this.serviceProviderLocation.getLatitude(),this.serviceProviderLocation.getLongitude());
+
+    void showRoute() {
+        //     LatLng serviceProviderLocation=new LatLng(this.serviceProviderLocation.getLatitude(),this.serviceProviderLocation.getLongitude());
         LatLng currentLocation = new LatLng(this.currentLocation.getLatitude(), this.currentLocation.getLongitude());
-        LatLng serviceProviderLocation=new LatLng(currentLocation.latitude+10,currentLocation.longitude+10);
-
-
+        LatLng serviceProviderLocation = new LatLng(currentLocation.latitude + 0.005, currentLocation.longitude + 0.005);
+        mMap.addMarker(new MarkerOptions().position(serviceProviderLocation));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(serviceProviderLocation,13));
+        Polyline route = mMap.addPolyline(new PolylineOptions().add(currentLocation, serviceProviderLocation));
+        route.setClickable(true);
+        route.setTag("Route");
+        route.setGeodesic(true);
+      
     }
-    void getServiceProviderLocation(String serviceProviderID){
-        LocationService locationService=new LocationService();
+
+    void getServiceProviderLocation(String serviceProviderID) {
+        LocationService locationService = new LocationService();
         locationService.getLocationOfActiveUser("sp", serviceProviderID, new LocationService.LocationTask() {
             @Override
             public void onGetDistance(String value) {
@@ -123,11 +141,12 @@ public class TrackOrder extends AppCompatActivity {
 
             @Override
             public void onSuccess(Object location) {
-                    serviceProviderLocation=(CustomLatLng) location;
-                    showRoute();
+                serviceProviderLocation = (CustomLatLng) location;
+                showRoute();
             }
         });
     }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
