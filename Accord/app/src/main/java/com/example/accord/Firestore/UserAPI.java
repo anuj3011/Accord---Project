9e313ab54744adaad5050e5d7b91c924e1ceb0da
Dhruv <dhruv_devasthale@outlook.com>
2020-11-21 16:51:44 +0530

    add null checks for user id

--- Accord/app/src/main/java/com/example/accord/Firestore/UserAPI.java
+++ Accord/app/src/main/java/com/example/accord/Firestore/UserAPI.java
@@ -5,26 +5,16 @@ import android.util.Log;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
-import com.example.accord.Models.CustomUser;
 import com.example.accord.Models.NGO;
 import com.example.accord.Models.ServiceProvider;
 import com.example.accord.Models.User;
-import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firebase.firestore.SetOptions;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class UserAPI {
     FirebaseFirestore db = FirebaseFirestore.getInstance();
@@ -39,67 +29,82 @@ public class UserAPI {
         void onFailure(String msg);
     }
 
+    boolean validateString(String s) {
+        if (s != null && s.length() > 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     public void getUser(final String type, String uid, final UserTask userTask) {
-       db.collection(type)// collection reference
-                .document(uid)
-                .addSnapshotListener(new EventListener<DocumentSnapshot>() {
-                    @Override
-                    public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException error) {
-                        if(error==null){
-                            if (type.equals("user")) {
-                                User user = documentSnapshot.toObject(User.class);
-                                if(user!=null){
-                                    userTask.onSuccess(user);
-                                }
-                                else {
-                                    userTask.onFailure("Could not get User");
-                                }
+        if (validateString(type) && validateString(uid)) {
+            db.collection(type)// collection reference
+                    .document(uid)
+                    .addSnapshotListener(new EventListener<DocumentSnapshot>() {
+                        @Override
+                        public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException error) {
+                            if (error == null) {
+                                if (type.equals("user")) {
+                                    User user = documentSnapshot.toObject(User.class);
+                                    if (user != null) {
+                                        userTask.onSuccess(user);
+                                    } else {
+                                        userTask.onFailure("Could not get User");
+                                    }
 
-                            } else if (type.equals("sp")) {
+                                } else if (type.equals("sp")) {
 
-                                ServiceProvider user = documentSnapshot.toObject(ServiceProvider.class);
-                                if(user!=null){
-                                    userTask.onSuccess(user);
-                                }
-                                else {
-                                    userTask.onFailure("Could not get User");
-                                }
+                                    ServiceProvider user = documentSnapshot.toObject(ServiceProvider.class);
+                                    if (user != null) {
+                                        userTask.onSuccess(user);
+                                    } else {
+                                        userTask.onFailure("Could not get User");
+                                    }
 
+                                } else {
+                                    NGO user = documentSnapshot.toObject(NGO.class);
+                                    if (user != null) {
+                                        userTask.onSuccess(user);
+                                    } else {
+                                        userTask.onFailure("Could not get User");
+                                    }
+                                }
                             } else {
-                                NGO user = documentSnapshot.toObject(NGO.class);
-                                if(user!=null){
-                                    userTask.onSuccess(user);
-                                }
-                                else {
-                                    userTask.onFailure("Could not get User");
-                                }
+                                userTask.onFailure(error.getMessage());
                             }
                         }
-                        else{
-                            userTask.onFailure(error.getMessage());
-                        }
-                    }
-                });
+                    });
+        }
+        else {
+            userTask.onFailure("Empty User ID");
+        }
+
 
     }
 
 
     public void pushUser(String type, String uid, final Object object, final UserTask userTask) {
         // Add a new document with a generated ID
-
-        db.collection(type)
-                .document(uid)
-                .set(object,SetOptions.merge()).addOnCompleteListener(new OnCompleteListener<Void>() {
-            @Override
-            public void onComplete(@NonNull Task<Void> task) {
-                if (task.isSuccessful()) {
-                    userTask.onSuccess(object);
-                    Log.d("document", "added");
-                } else {
-                    userTask.onFailure(task.getException().getMessage());
-                    Log.d("document", task.getException().getMessage());
+        if(validateString(type) && validateString(uid)){
+            db.collection(type)
+                    .document(uid)
+                    .set(object, SetOptions.merge()).addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    if (task.isSuccessful()) {
+                        userTask.onSuccess(object);
+                        Log.d("document", "added");
+                    } else {
+                        userTask.onFailure(task.getException().getMessage());
+                        Log.d("document", task.getException().getMessage());
+                    }
                 }
-            }
-        });
+            });
+        }
+        else {
+            userTask.onFailure("Empty User ID");
+        }
+
     }
 }
